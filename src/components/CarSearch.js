b9cb4802676668e42214e8bf992bbe6636c3cc8f
Nidhinsha <nidhinsha64@gmail.com>
2023-03-04 20:31:49 +0530

    search completed

--- src/components/CarSearch.js
+++ src/components/CarSearch.js
@@ -1,8 +1,30 @@
 import React from 'react'
-
+import { useDispatch, useSelector } from 'react-redux'
+import {changeSearchTerm} from '../store'
 function CarSearch() {
+
+    const dispatch = useDispatch()
+
+    const searchTerm = useSelector((state)=>{
+        return state.cars.searchTerm
+    })
+
+    const handleSearchTermChange = (event) => {
+        dispatch(changeSearchTerm(event.target.value))
+    }
   return (
-    <div>
+    <div className='list-header'>
+        <h3 className="titile is-3">My Cars</h3>
+        <div className="search field is-horizontal">
+            <label htmlFor="" className="label">
+                search
+            </label>
+            <input 
+                type="text"
+                className="input"
+                value={searchTerm}
+                onChange={handleSearchTermChange} />
+        </div>
       car search
     </div>
   )
